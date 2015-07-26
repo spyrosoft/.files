032109abbc15cf1347383279dbd3ffa1be3d13f4
Bennett Roesch <meta@spyrosoft.com>
2015-07-26 15:01:43 -0700

    Customize colors of rainbow delimiters. Make them drastically more contrasty than the defaults.

--- .emacs.d/extra/packages.el
+++ .emacs.d/extra/packages.el
@@ -62,6 +62,17 @@
 (require 'rainbow-delimiters)
 (add-hook 'lisp-mode-hook #'rainbow-delimiters-mode)
 (add-hook 'emacs-lisp-mode-hook #'rainbow-delimiters-mode)
+(custom-set-faces
+ '(rainbow-delimiters-depth-1-face ((((class color)) (:foreground "cyan"))))
+ '(rainbow-delimiters-depth-2-face ((((class color)) (:foreground "blue"))))
+ '(rainbow-delimiters-depth-3-face ((((class color)) (:foreground "dark orange"))))
+ '(rainbow-delimiters-depth-4-face ((((class color)) (:foreground "spring green"))))
+ '(rainbow-delimiters-depth-5-face ((((class color)) (:foreground "dark red"))))
+ '(rainbow-delimiters-depth-6-face ((((class color)) (:foreground "yellow"))))
+ '(rainbow-delimiters-depth-7-face ((((class color)) (:foreground "dark magenta"))))
+ '(rainbow-delimiters-depth-8-face ((((class color)) (:foreground "green"))))
+ '(rainbow-delimiters-depth-9-face ((((class color)) (:foreground "red"))))
+ )
 
 ;; M-x list-packages
 ;; Install ace-window
