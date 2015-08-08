54ecd74d5fb589bd84d98cc7106f18988f7b696c
Bennett Roesch <meta@spyrosoft.com>
2015-08-08 14:00:56 -0700

    Add iy-go-to-char.el to packages.

--- .emacs.d/extra/packages.el
+++ .emacs.d/extra/packages.el
@@ -82,6 +82,14 @@
 ;; Jump to visible character in any window
 (global-set-key (kbd "M-C-:") 'avy-goto-char)
 
+;; Jump to next matching character
+;; http://www.emacswiki.org/emacs/iy-go-to-char.el
+(add-to-list 'load-path "~/.emacs.d/extra/iy-go-to-char.el")
+(require 'iy-go-to-char)
+(global-set-key (kbd "C-x j") 'iy-go-up-to-char)
+(global-set-key (kbd "C-x J") 'iy-go-up-to-char-backward)
+
+
 
 ;; ----------Built In----------
 
