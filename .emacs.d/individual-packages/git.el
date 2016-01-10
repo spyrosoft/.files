ac0f1691430e7195f85e709333bd690f44b05db0
Bennett Roesch <meta@spyrosoft.com>
2016-01-10 00:31:56 -0800

    The dash package is required by the git package.

--- .emacs.d/individual-packages/git.el
+++ .emacs.d/individual-packages/git.el
@@ -1,5 +1,5 @@
 ;; https://github.com/magnars/dash.el.git
-;(add-to-list 'load-path "~/.emacs.d/packages/dash.el")
+(add-to-list 'load-path "~/.emacs.d/packages/dash.el")
 ;; https://github.com/magit/magit/releases
 (add-to-list 'load-path "~/.emacs.d/packages/magit")
 (eval-after-load 'info
