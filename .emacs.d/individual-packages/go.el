e65dc5621e5fd64cfba3232af2c25d1bb910bfbd
Bennett Roesch <meta@spyrosoft.com>
2016-11-17 13:34:03 -0800

    Add go fmt on save to emacs config.

--- .emacs.d/individual-packages/go.el
+++ .emacs.d/individual-packages/go.el
@@ -1,3 +1,7 @@
 ;; https://github.com/dominikh/go-mode.el
 (add-to-list 'load-path "~/.emacs.d/packages/go-mode.el")
 (require 'go-mode-autoloads)
+
+;; https://godoc.org/golang.org/x/tools/cmd/goimports
+(setq gofmt-command "goimports")
+(add-hook 'before-save-hook 'gofmt-before-save)
